3df794f98efa0af2ce6390f439518eb3d8ee9a25
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-08-06 11:33:45 +0100

    Files and Stream(initial practice)

---
+++ test/FileOnePracticesTest/PracticeTest.java
@@ -0,0 +1,16 @@
+package FileOnePracticesTest;
+
+import FileOnePractices.Practice;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PracticeTest {
+    Practice practice =new Practice();
+//    @Test
+//    void practiceCanCreateAFileObject(){
+//        assertTrue(practice.isDrectory())
+//    }
+}
